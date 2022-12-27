51bb26cca89810bf23feac6703c0757ae75eea9b
Ilias Elbadaoui <iliassovich@outlook.com>
2022-12-27 11:20:15 +0100

    last --ver

--- aws_crash.sh
+++ aws_crash.sh
@@ -1,4 +1,10 @@
+# kubectl delete pods -n vicles-app --all
+# kubectl delete deployments -n vicles-app  --all
+# kubectl delete svc -n vicles-app  --all
+# kubectl delete pvc -n vicles-app  --all
+
 kubectl delete pods --all
 kubectl delete deployments --all
 kubectl delete svc --all
 kubectl delete pvc --all
+# kubectl delete namespace vicles-app
--- aws_setup.sh
+++ aws_setup.sh
@@ -1,5 +1,8 @@
+# kubectl create namespace vicles-app
 kubectl apply -f k8s/mysql.yml
 kubectl apply -f k8s/pma.yml
 kubectl apply -f k8s/app.yml
 kubectl apply -f k8s/api.yml
 kubectl apply -f k8s/caddy.yml
+
+# arn:aws:iam::717189823417:policy/AWLLBCIAMPolicy
